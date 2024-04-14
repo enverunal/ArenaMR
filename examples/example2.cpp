dad337a27ce2c8683184b5bcd3758cfbadfb406e
Enver Nuri Ünal <enverabc@gmail.com>
2024-04-14 18:38:48 +0300

    Example2 updated.

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -14,23 +14,19 @@ int main()
     using namespace std::chrono;
 
     {
-        arena_mr::UnsynchronizedArenaMR memory_resource(10, 10'000); // tune your arena
+        arena_mr::UnsynchronizedArenaMR memory_resource(10, 100'000); // tune your arena
 
         steady_clock::time_point begin = steady_clock::now();
 
         {
-            std::pmr::vector<std::string> x1(&memory_resource);
-            std::pmr::unordered_map<std::string, std::vector<std::string>> x2(&memory_resource);
-            std::pmr::map<std::string, std::vector<std::string>> x3(&memory_resource);
-            for (int i = 0; i < 100'000; ++i)
+            std::pmr::map<std::string, std::vector<std::string>> v(&memory_resource);
+            for (int i = 0; i < 1'000'000; ++i)
             {
-                x1.emplace_back(std::to_string(i));
-                x2.emplace(std::to_string(i), std::vector{std::to_string(i)});
-                x3.emplace(std::to_string(i), std::vector{std::to_string(i)});
+                v.emplace(std::to_string(i), std::vector{std::to_string(i)});
             }
         }
         steady_clock::time_point end = steady_clock::now();
-        std::cout << "Time difference = " << duration_cast<nanoseconds>(end - begin).count() << "[ns]" << std::endl;
+        std::cout << "UnsynchronizedArenaMR: " << duration_cast<nanoseconds>(end - begin).count() << "[ns]" << std::endl;
     }
 
     {
@@ -39,17 +35,29 @@ int main()
         steady_clock::time_point begin = steady_clock::now();
 
         {
-            std::pmr::vector<std::string> x1(memory_resource);
-            std::pmr::unordered_map<std::string, std::vector<std::string>> x2(memory_resource);
-            std::pmr::map<std::string, std::vector<std::string>> x3(memory_resource);
-            for (int i = 0; i < 100'000; ++i)
+            std::pmr::map<std::string, std::vector<std::string>> v(memory_resource);
+            for (int i = 0; i < 1'000'000; ++i)
             {
-                x1.emplace_back(std::to_string(i));
-                x2.emplace(std::to_string(i), std::vector{std::to_string(i)});
-                x3.emplace(std::to_string(i), std::vector{std::to_string(i)});
+                v.emplace(std::to_string(i), std::vector{std::to_string(i)});
             }
         }
         steady_clock::time_point end = steady_clock::now();
-        std::cout << "Time difference = " << duration_cast<nanoseconds>(end - begin).count() << "[ns]" << std::endl;
+        std::cout << "new_delete_resource: " << duration_cast<nanoseconds>(end - begin).count() << "[ns]" << std::endl;
+    }
+
+    {
+        std::pmr::monotonic_buffer_resource memory_resource(1'000'000);
+
+        steady_clock::time_point begin = steady_clock::now();
+
+        {
+            std::pmr::map<std::string, std::vector<std::string>> v(&memory_resource);
+            for (int i = 0; i < 1'000'000; ++i)
+            {
+                v.emplace(std::to_string(i), std::vector{std::to_string(i)});
+            }
+        }
+        steady_clock::time_point end = steady_clock::now();
+        std::cout << "monotonic_buffer_resource: " << duration_cast<nanoseconds>(end - begin).count() << "[ns]" << std::endl;
     }
 }
